4a43da9ead3120008cb5aac24fdce54c07d9edf2
LaurenG <laurengarelick@gmail.com>
2020-05-04 03:35:27 -0700

    additions of markdown template

--- HW_9/index.js
+++ HW_9/index.js
@@ -3,7 +3,7 @@ const util = require('util');
 const {prompt} = require('inquirer');
 const fs = require('fs');
 
-https://img.shields.io/redmine/plugin/stars/laurengarelick.io?color=purple&logo=purple
+
 // * At least one badge
 // * Project title
 // * Description
@@ -18,62 +18,143 @@ https://img.shields.io/redmine/plugin/stars/laurengarelick.io?color=purple&logo=
 // * User GitHub email
 
 fs.writeFile = util.promisify(fs.writeFile);
-const questions = [
-    {
-        message: "What is your name?",
-        name: 'name',
-        default: 'chad'
-    },
-    {
-        message: "What is your location?",
-        name: 'location',
-        default: 'home'
-    },
-    {
-        message: "Tell me a bit about yourself",
-        name: 'bio',
-        default: 'im cool'
-    },
-    {
-        message: "What is your linkedin url?",
-        name: 'linkedin',
-        default: 'coolguy.linkedin'
-    },
-    {
-        message: "What do you like to eat for lunch?",
-        name: 'lunch',
-        default: 'meat'
-    },
-    {
-        message: "What is your birthday?",
-        name: 'bday',
-        default: 'null'
-    },
-]
+const questions = [{
+    message: "What is the title of your project?",
+    name: 'title',
+    default: "The Best Project Ever"
+},
+{
+    message: "Describe your project",
+    name: 'description',
+    default: "This project does lots of cool things."
+},
+{
+    type: 'checkbox',
+    message: "What would you like to include in your table of contents?",
+    name: 'toc',
+    choices: ['Installation', 'Useage', 'Credits', 'License '],
+    default: [""]
+    
+},
+{
+    message: "What are the steps required to install your project?",
+    name: 'install',
+    default: 'No installation required.'
+},
+{
+    message: "Please provide instructions and examples for use.",
+    name: 'usage',
+    default: ''
+},
+{
+    message: "Please list the GitHub usernames of all those who contributed to this project.",
+    name: 'gitHubEmail',
+    default: 'laurengarelick@gmail.com'
+},
+{
+    message: "License?",
+    name: 'license',
+    default: 'MIT'
+},
+{
+    message: "Would you like to add any badges to this project? If so, please list the URLs seperated by a comma to any badges.",
+    name: 'badge',
+    default: 'https://img.shields.io/redmine/plugin/stars/laurengarelick.io?color=purple&logo=purple'
+},
+{
+    
+    message: "Are there any tests you'd like to include with this project?",
+    name: 'test',
+    default: "No tests at this time."
+}]
+
+
+async function init(){
+    const answers = await prompt(questions);
+    const template = `<!DOCTYPE md>
+
+    `
+    fs.writeFile(`${answers.name}Profile.html`, template, (err)=>console.log('success!'))
+}
+init()
 //use prompt to gather some info...
 async function init(){
     const response = await prompt(questions);
     console.log(response);
     writeHTML(response)
 }
-async function writeHTML(data){
-const template = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hello ${data.name}!</h1>
-    <h2>you live at ${data.location}</h2>
-    <h2>about you - ${data.bio}</h2>
-    <h2>linkedin - ${data.linkedin}</h2>
-    <h2>having ${data.lunch} for lunch</h2>
-    <h2>birthday is on ${data.bday}</h2>
-</body>
-</html>`
-   await fs.writeFile(`${data.name}.html`, template);
+async function writeMD (data){
+const template = 
+`# Title of Project
+
+## Description
+
+## Table of Contents
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests`
+   await fs.writeFile(`${data.name}.md`, template);
    console.log('yay file created!')
 }
 init()
+
+if (github_username !== "") {
+
+    const github_query = `https://api.github.com/users/${github_username}/events/public`;
+
+    axios.get(github_query).then(function(github_userdata) {
+
+    //user email address:
+    const gitHubEmail = getEmailAddress(github_userdata);
+    console.log(`User's Email address: ${gitHubEmail}`);
+
+    //user profile image:
+    const gitHubProfileImage = getProfileImage(github_userdata);
+    console.log(`User's Profile Image: ${gitHubProfileImage}`);
+
+    });
+
+};
+
+function createBadge(type, title) {
+return `https://img.shields.io/badge/${type}-${title}-blue`;
+}
+
+function getEmailAddress(github_userdata) {
+
+for (let i = 0; i < github_userdata.data.length; i++) {
+
+    if (github_userdata.data[i].payload.hasOwnProperty("commits")) {
+        available = true;
+        const gitHubEmail = github_userdata.data[i].payload.commits[0].author.email;
+        return gitHubEmail;
+    }
+
+}
+
+return "User profile email is unavailable";
+
+
+}
+
+function getProfileImage(github_userdata) {
+
+for (let i = 0; i < github_userdata.data.length; i++) {
+
+    if (github_userdata.data[i].actor.hasOwnProperty("avatar_url")) { 
+        const gitHubProfileImage = github_userdata.data[i].actor.avatar_url;
+        return gitHubProfileImage;
+    }
+
+}
+
+return "User profile image unavailable";
+
+}
